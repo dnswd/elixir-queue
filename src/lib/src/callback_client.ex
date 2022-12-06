9f12de5ef188a482129b27a25964a322be287b99
Taufiq Hadi Pratama <taufiqhadipratama@gmail.com>
2022-12-06 16:48:34 +0700

    Added parameter topic for consumer API

--- src/lib/src/callback_client.ex
+++ src/lib/src/callback_client.ex
@@ -3,13 +3,19 @@ defmodule Src.CallbackClient do
 
   def send_callback(url, body) do
     headers = [{"Content-type", "application/json"}]
-    case post(url, Poison.encode!(%{message: body}), headers, []) do
+
+    case post(url, Poison.encode!(body), headers, []) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        IO.puts body
+        IO.puts(body)
+
       {:ok, %HTTPoison.Response{status_code: 404}} ->
-        IO.puts "Not found"
+        IO.puts("Not found")
+
       {:error, %HTTPoison.Error{reason: reason}} ->
-        IO.inspect reason
+        IO.inspect(reason)
+
+      {:ok, %HTTPoison.Response{status_code: 500}} ->
+        IO.puts("Consumer`s callback server is down")
     end
   end
 end
