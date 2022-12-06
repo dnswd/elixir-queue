bcb2aaca4c8b9e2de81bddbad1c2e0b1b4f9e8c7
Taufiq Hadi Pratama <taufiqhadipratama@gmail.com>
2022-12-06 12:38:56 +0700

    Added CORS policy

--- src/mix.exs
+++ src/mix.exs
@@ -50,7 +50,8 @@ defmodule Src.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:httpoison, "~> 1.8"},
-      {:poison, "~> 5.0"}
+      {:poison, "~> 5.0"},
+      {:cors_plug, "~> 3.0"}
     ]
   end
 
